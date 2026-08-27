550ede9e1be6d8fc3ffea84aca35dda661e2663f
xFlippy <94675097+xflipperkast@users.noreply.github.com>
2026-08-27 23:56:08 +0200

    Delete source directory

--- source/crash_log.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdarg.h>
-
-void crash_log_printf(const char *fmt, ...) __attribute__((format(printf,1,2)));
-void crash_log_vprintf(const char *fmt, va_list ap);
-void crash_log_flush(void);
-void crash_log_close(void);
--- source/fatal.h
+++
@@ -1,4 +0,0 @@
-#ifndef ASNX_FATAL_H
-#define ASNX_FATAL_H
-void fatal_error(const char *fmt, ...) __attribute__((format(printf,1,2),noreturn));
-#endif
