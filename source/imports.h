7746340c22185cb5082f260f8a380c70a375100b
xFlippy <94675097+xflipperkast@users.noreply.github.com>
2026-08-30 13:27:34 +0200

    Delete source directory

--- source/imports.h
+++
@@ -1,9 +0,0 @@
-#ifndef ASNX_IMPORTS_H
-#define ASNX_IMPORTS_H
-#include <stdint.h>
-#include "so_util.h"
-void imports_init(void);
-uintptr_t imports_lookup(const char *name);
-int imports_resolve_module(so_module *m);
-uintptr_t imports_fallback(void);
-#endif
